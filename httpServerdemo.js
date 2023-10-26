fc60d3a0f28f0a32b2449e90fc67a1562b42398a
Rajat Sharma <rajatxd007@gmail.com>
2023-10-27 01:01:10 +0530

    Added endpoints to servers

--- httpServerdemo.js
+++ httpServerdemo.js
@@ -3,16 +3,35 @@ const http = require('http');
 const port = 3000;
 
 const server = http.createServer((req,res)=>{
-    res.writeHead(200 , {
-        'Content-Type' : 'application/json'
-    });
-    res.end(JSON.stringify({
-        id : 1,
-        name : 'Issac Newton',
+    // console.log('req fired is', req);
+
+    //adding condition for two different resurces and throwing 404 for resource not found
+    if(req.url === '/friends'){
+        res.statusCode = 200;
+        res.setHeader('Content-Type' , 'application/JSON')
+        res.end(JSON.stringify({
+            id : 1,
+            name : 'Issac Newton',
     }))
+    }else if(req.url === '/messages'){
+        res.statusCode = 200;
+        res.setHeader('Content-Type' , 'application/html');
+        res.write('<!DOCTYPE html>')
+        res.write('<html>')
+        res.write('<body>')
+        res.write('<h1>My First HTML API Response</h1>')
+        res.write('<p>My first html object</p>')
+        res.write('</body>')
+        res.write('</html>')
+        res.end();
+    }else{
+        res.statusCode=404;
+        res.end();
+    }
+    
 });
 
 
 server.listen(port , ()=>{
     console.log(`listening on port ${port}`);
-});
+}); 
