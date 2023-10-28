b9e67d09ae128cbdef4d5d5224e50514c0db07f8
Rajat Sharma <rajatxd007@gmail.com>
2023-10-28 20:59:42 +0530

    Added code for post request on /friends resource

--- httpServerdemo.js
+++ httpServerdemo.js
@@ -1,19 +1,25 @@
 const http = require('http');
 
 const port = 3000;
+const friendList = [{id : 1,name : 'Issac Newton',}];
 
 const server = http.createServer((req,res)=>{
     // console.log('req fired is', req);
-
     //adding condition for two different resurces and throwing 404 for resource not found
-    if(req.url === '/friends'){
+    if(req.method === 'POST' && req.url === '/friends'){
+        req.on('data',(data)=>{
+            //console.log(`request is ${data} ${typeof data}`); 
+            const friendsData = data.toString();
+            console.log('data is ',friendsData);
+            friendList.push(JSON.parse(friendsData));
+        });
+        console.log(`friendList is ${friendList}`);
+    }
+    else if(req.method ==='GET' && req.url === '/friends'){
         res.statusCode = 200;
         res.setHeader('Content-Type' , 'application/JSON')
-        res.end(JSON.stringify({
-            id : 1,
-            name : 'Issac Newton',
-    }))
-    }else if(req.url === '/messages'){
+        res.end(JSON.stringify(friendList));
+    }else if(req.method ==='GET' && req.url === '/messages'){
         res.statusCode = 200;
         res.setHeader('Content-Type' , 'application/html');
         res.write('<!DOCTYPE html>')
